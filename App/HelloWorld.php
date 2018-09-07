08075b3b3c25ec85d508c3313fdd1a439bcecbfd
renatoabreu14 <renato.abreu.info@gmail.com>
2018-09-06 21:55:27 -0300

    Iniciando biblioteca

---
+++ App/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renato
+ * Date: 06/09/18
+ * Time: 21:52
+ */
+
+namespace App;
+
+
+class HelloWorld{
+
+    public function getHello(){
+        return "Hello World!";
+    }
+}
